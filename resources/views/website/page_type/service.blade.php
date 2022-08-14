f676b60bf16ba7b7593d7037355e19abef0b9e99
md <mddasgudiya@gmail.com>
2022-08-14 15:46:27 +0545

    services

--- resources/views/website/page_type/service.blade.php
+++ resources/views/website/page_type/service.blade.php
@@ -21,92 +21,24 @@
                 <h2 class="text-bold">Our&nbsp;Services</h2>
                 <hr class="divider bg-madison">
                 <div class="row range-30 isotope offset-top-60 text-left">
-                    <div class="col-xs-12 col-sm-6 col-md-4 isotope-item">
-                        <article class="post-course"><a href="course-details.html"><img class="img-responsive"
-                                    src="https://images.pexels.com/photos/1326947/pexels-photo-1326947.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                                    width="370" height="240" alt=""></a>
-                            <div class="post-news-body">
-                                <h6><a href="services-detail.html">Career Counselling</a></h6>
+                    <!---foreach start---->
+                    @foreach($services as $service)
+                        <div class="col-xs-12 col-sm-6 col-md-4 isotope-item">
+                            <article class="post-course"><a href="course-details.html"><img class="img-responsive"
+                                        src="{{$service->banner_image}}"
+                                        width="370" height="240" alt=""></a>
+                                <div class="post-news-body">
+                                    <h6><a href="services-detail.html">{{$service->caption}}</a></h6>
 
-                                <div class="offset-top-10">
-                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, rem.</p>
+                                    <div class="offset-top-10">
+                                        <p>{{$service->short_content}}</p>
+                                    </div>
+
                                 </div>
-
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 col-md-4 isotope-item">
-                        <article class="post-news"><a href="news-post-page.html"><img class="img-responsive"
-                                    src="https://images.pexels.com/photos/1698485/pexels-photo-1698485.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                                    width="370" height="240" alt=""></a>
-                            <div class="post-news-body">
-                                <h6><a href="services-detail.html">Complete Information About Educational
-                                        Institution</a></h6>
-
-                                <div class="offset-top-10">
-                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, rem.</p>
-                                </div>
-
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 col-md-4 isotope-item">
-                        <article class="post-news"><a href="news-post-page.html"><img class="img-responsive"
-                                    src="https://images.pexels.com/photos/159866/books-book-pages-read-literature-159866.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                                    width="370" height="240" alt=""></a>
-                            <div class="post-news-body">
-                                <h6><a href="services-detail.html">Documentation Assistance</a></h6>
-
-                                <div class="offset-top-10">
-                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, rem.</p>
-                                </div>
-
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 col-md-4 isotope-item">
-                        <article class="post-news"><a href="news-post-page.html"><img class="img-responsive"
-                                    src="https://images.pexels.com/photos/258353/pexels-photo-258353.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                                    width="370" height="240" alt=""></a>
-                            <div class="post-news-body">
-                                <h6><a href="services-detail.html">Language Classes By Expert Instructors</a></h6>
-
-                                <div class="offset-top-10">
-                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, rem.</p>
-                                </div>
-
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 col-md-4 isotope-item">
-                        <article class="post-news"><a href="news-post-page.html"><img class="img-responsive"
-                                    src="https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                                    width="370" height="240" alt=""></a>
-                            <div class="post-news-body">
-                                <h6><a href="services-detail.html">Career Counselling
-                                    </a></h6>
-
-                                <div class="offset-top-10">
-                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, rem.</p>
-                                </div>
-
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 col-md-4 isotope-item">
-                        <article class="post-news"><a href="news-post-page.html"><img class="img-responsive"
-                                    src="https://images.pexels.com/photos/4145153/pexels-photo-4145153.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                                    width="370" height="240" alt=""></a>
-                            <div class="post-news-body">
-                                <h6><a href="services-detail.html">Combined with a Handful.</a></h6>
-
-                                <div class="offset-top-10">
-                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, rem.</p>
-                                </div>
-
-                            </div>
-                        </article>
-                    </div>
+                            </article>
+                        </div>
+                    @endforeach    
+                    <!---stop foreach---->
                 </div>
 
             </div>
