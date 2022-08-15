ba52fda85b3f8082120a2b29a676f8c8671a41a7
md <mddasgudiya@gmail.com>
2022-08-15 11:50:27 +0545

    Video gallery

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -352,6 +352,7 @@ class HomeController extends Controller
      public function videogallery($slug){
         $navigataion_id = Navigation::where('nav_name',$slug)->first()->id;        
         $photos = NavigationVideoItems::where('navigation_id',$navigataion_id)->get();
+        return $photos;
         //return $photos;
          $global_setting = GlobalSetting::all()->first(); 
          $menus = Navigation::query()->where('nav_category','Main')->where('page_type','!=','Job')->where('page_type','!=','Photo Gallery')->where('page_type','!=','Notice')->where('parent_page_id',0)->where('page_status','1')->orderBy('position','ASC')->get();
