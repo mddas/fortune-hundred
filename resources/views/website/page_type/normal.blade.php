bf63fe864f0386e2e2e845c9f2a121eeb1cbe401
md <mddasgudiya@gmail.com>
2022-08-15 12:37:06 +0545

    slug

--- resources/views/website/page_type/normal.blade.php
+++ resources/views/website/page_type/normal.blade.php
@@ -11,7 +11,7 @@
 
           <ul class="list-inline list-inline-lg list-inline-dashed p">
             <li><a href="#">{{$slug1 ?? ''}}</a></li>
-            <li>{{$slug_detail->first()->nav_name ?? ''}}</li>
+            <li>{{$slug_detail->nav_name ?? ''}}</li>
           </ul>
 
         </div>
