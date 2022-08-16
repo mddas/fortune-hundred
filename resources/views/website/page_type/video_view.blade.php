e4b0201faba1b4b50d315af53afd961be5e59108
md <mddasgudiya@gmail.com>
2022-08-16 17:40:25 +0545

    gallery

--- resources/views/website/page_type/video_view.blade.php
+++ resources/views/website/page_type/video_view.blade.php
@@ -3,37 +3,118 @@
 
 	@extends('layouts.master')
     @section("content")
-			<div class="theme-inner-banner">
-				<div class="overlay">
-					<div class="container">
-						<h2>Video Gallery</h2>
-					</div> <!-- /.container -->
-				</div> <!-- /.overlay -->
-			</div> <!-- /.theme-inner-banner -->
+	<section class="section breadcrumb-modern context-dark parallax-container" style="background-image:   linear-gradient(
+                              rgba(27, 40, 92, 0.7),
+                              rgba(28, 35, 61, 0.7)
+                            ) ,url(images/gallery/gallery.webp)">
+            <div class="parallax-content section-30 section-sm-70">
+                <div class="shell">
+                    <h2 class="veil reveal-sm-block">Video Gallery</h2>
+                    <div class="offset-sm-top-35">
+                        <ul class="list-inline list-inline-lg list-inline-dashed p">
+                            <li><a href="index.html">Home</a></li>
 
+                            <li>Video Gallery</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section novi-background bg-cover section-70 section-md-114 bg-default">
+            <div class="shell">
+                <hr class="divider bg-madison" />
+                <div class="offset-top-60">
+                    <div class="range range-30 range-xs-center" data-lightgallery="group">
+                        <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                            <div class=" video_gallery">
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/Ps54W1CPCF0"
+                                    title="YouTube video player" frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                                    <a href="#">Travel</a>
+                            </div>
+                        </div>
 
-			<!-- 
-			=============================================
-				gallery
-			============================================== 
-			-->
-			@if(isset($photos))
-			<section class="section-spacing">
-				<div class="container">
-					<div class="gallery-view">
-		                <div class="row" id="lightgallery"> 
-							<!----gallary data----->
-							@foreach($photos as $photo)
-								<iframe width="420" height="315"
-									src="{{$photo->link}}">
-								</iframe>
-							@endforeach
-						  <!----gallary data close----->
-		                </div>
-		            </div>
-          		</div>
-			</section>
-		  @endif
+                        
+                         <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                             <div class=" video_gallery">
+                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/Ps54W1CPCF0"
+                                     title="YouTube video player" frameborder="0"
+                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                     allowfullscreen></iframe>
+                                 <a href="#">Travel</a>
+                             </div>
+                         </div>
+                         <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                             <div class=" video_gallery">
+                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/Ps54W1CPCF0"
+                                     title="YouTube video player" frameborder="0"
+                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                     allowfullscreen></iframe>
+                                 <a href="#">Travel</a>
+                             </div>
+                         </div>
+                         <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                             <div class=" video_gallery">
+                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/Ps54W1CPCF0"
+                                     title="YouTube video player" frameborder="0"
+                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                     allowfullscreen></iframe>
+                                 <a href="#">Travel</a>
+                             </div>
+                         </div>
+                         <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                             <div class=" video_gallery">
+                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/Ps54W1CPCF0"
+                                     title="YouTube video player" frameborder="0"
+                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                     allowfullscreen></iframe>
+                                 <a href="#">Travel</a>
+                             </div>
+                         </div>
+                         <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                             <div class=" video_gallery">
+                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/Ps54W1CPCF0"
+                                     title="YouTube video player" frameborder="0"
+                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                     allowfullscreen></iframe>
+                                 <a href="#">Travel</a>
+                             </div>
+                         </div>
+                         <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                             <div class=" video_gallery">
+                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/Ps54W1CPCF0"
+                                     title="YouTube video player" frameborder="0"
+                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                     allowfullscreen></iframe>
+                                 <a href="#">Travel</a>
+                             </div>
+                         </div>
+                         <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                             <div class=" video_gallery">
+                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/Ps54W1CPCF0"
+                                     title="YouTube video player" frameborder="0"
+                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                     allowfullscreen></iframe>
+                                 <a href="#">Travel</a>
+                             </div>
+                         </div>
+                         <div class="cell-xs-10 cell-sm-6 cell-md-4">
+                             <div class=" video_gallery">
+                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/Ps54W1CPCF0"
+                                     title="YouTube video player" frameborder="0"
+                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                     allowfullscreen></iframe>
+                                 <a href="#">Travel</a>
+                             </div>
+                         </div>
+
+                        
+
+                    </div>
+                </div>
+            </div>
+        </section>
 		  
 		  <!-- <script>
 			  // $('#lightgallery').html('')
