0849f11cd22c8db0a7f22871b5e52c561b0ac243
md <mddasgudiya@gmail.com>
2022-08-16 16:50:09 +0545

    gallery

--- resources/views/website/page_type/album.blade.php
+++ resources/views/website/page_type/album.blade.php
@@ -45,4 +45,88 @@
                 </div>
             </div>
         </section><!-- Corporate footer-->
+
+        <!----yakendra--->
+
+
+        <section class="section breadcrumb-modern context-dark parallax-container" style="background-image:   linear-gradient(
+                              rgba(27, 40, 92, 0.7),
+                              rgba(28, 35, 61, 0.7)
+                            ) ,url(images/gallery/gallery.webp)">
+      <div class="parallax-content section-30 section-sm-70">
+        <div class="shell">
+          <h2 class="veil reveal-sm-block">Image Gallery</h2>
+          <div class="offset-sm-top-35">
+            <ul class="list-inline list-inline-lg list-inline-dashed p">
+              <li><a href="index.html">Home</a></li>
+
+              <li>Image Gallery</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section novi-background bg-cover section-70 section-md-114 bg-default">
+      <div class="shell">
+        <hr class="divider bg-madison" />
+        <div class="offset-top-60">
+          <div class="range range-30 range-xs-center" data-lightgallery="group">
+            <div class="cell-xs-10 cell-sm-6 cell-md-4">
+
+              <div class="image_gallery_photo">
+                <img src="images/gallery/gallery.webp" alt="" width="370" height="370" />
+                <a href="image-item.html">Travel</a>
+              </div>
+
+
+            </div>
+
+            <div class="cell-xs-10 cell-sm-6 cell-md-4">
+
+              <div class="image_gallery_photo">
+                <img src="images/gallery/gallery.webp" alt="" width="370" height="370" />
+                <a href="image-item.html">Travel</a>
+              </div>
+
+
+            </div>
+            <div class="cell-xs-10 cell-sm-6 cell-md-4">
+
+              <div class="image_gallery_photo">
+                <img src="images/gallery/gallery.webp" alt="" width="370" height="370" />
+                <a href="image-item.html">Travel</a>
+              </div>
+
+
+            </div>
+            <div class="cell-xs-10 cell-sm-6 cell-md-4">
+
+              <div class="image_gallery_photo">
+                <img src="images/gallery/gallery.webp" alt="" width="370" height="370" />
+                <a href="image-item.html">Travel</a>
+              </div>
+
+
+            </div>
+            <div class="cell-xs-10 cell-sm-6 cell-md-4">
+
+              <div class="image_gallery_photo">
+                <img src="images/gallery/gallery.webp" alt="" width="370" height="370" />
+                <a href="image-item.html">Travel</a>
+              </div>
+
+
+            </div>
+            <div class="cell-xs-10 cell-sm-6 cell-md-4">
+
+              <div class="image_gallery_photo">
+                <img src="images/gallery/gallery.webp" alt="" width="370" height="370" />
+                <a href="image-item.html">Travel</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
     @endsection
