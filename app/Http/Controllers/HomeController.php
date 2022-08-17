1e8d40a7087c9d4d0ee28cf123ebc7beeec3257d
md <mddasgudiya@gmail.com>
2022-08-17 11:03:31 +0545

    video gal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -304,11 +304,7 @@ class HomeController extends Controller
             return view("website.page_type.album")->with(['slug1'=>$slug1,'albumbs'=>$albumbs,'jobs'=>$jobs,'menus'=>$menus,'sliders'=>$sliders,'about'=>$About,'global_setting'=>$global_setting,'slug_detail'=>$slug_detail]);
         }
         if($subcategory_type == "Video Gallery"){//Albumb 
-            //return \DB::connection()->getDatabaseName();
-            return NavigationVideoItems::all();
             $photos = NavigationVideoItems::where('navigation_id',$subcategory_id)->get();
-            // return $subcategory_id;2433
-            return $photos;
             return view("website.page_type.video_view")->with(['photos'=>$photos,'slug1'=>$slug1,'jobs'=>$jobs,'menus'=>$menus,'sliders'=>$sliders,'about'=>$About,'global_setting'=>$global_setting,'slug_detail'=>$slug_detail]);
         }
         elseif($subcategory_type == "Service"){
