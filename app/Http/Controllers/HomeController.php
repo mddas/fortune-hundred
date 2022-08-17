fb8b8eee8b723887f48cc6980c18d382ab2fcf86
md <mddasgudiya@gmail.com>
2022-08-17 11:07:37 +0545

    video gal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -305,6 +305,7 @@ class HomeController extends Controller
         }
         if($subcategory_type == "Video Gallery"){//Albumb 
             $photos = NavigationVideoItems::where('navigation_id',$subcategory_id)->get();
+            return $photos;
             return view("website.page_type.video_view")->with(['photos'=>$photos,'slug1'=>$slug1,'jobs'=>$jobs,'menus'=>$menus,'sliders'=>$sliders,'about'=>$About,'global_setting'=>$global_setting,'slug_detail'=>$slug_detail]);
         }
         elseif($subcategory_type == "Service"){
